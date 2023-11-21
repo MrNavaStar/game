36b15ad8dbae2fcd2e887f7b145ed558038247f3
Neshko Janjic <janjicn2@students.cs.macewan.ca>
2023-11-21 09:51:02 -0700

    adding in pause and quit screens

--- src/code/main.c
+++ src/code/main.c
@@ -3,7 +3,16 @@
 #include "../headers/screens.h"
 #include "../headers/levels.h"
 
+typedef struct{
+   int x;
+   int y;
+   char ch;
+   int level;
+}Player;
+
+
 int main() {
+    int ch;
     WINDOW *w;
     setlocale(LC_ALL, ""); // Set locale so special chars print correctly
     initscr();
@@ -11,19 +20,38 @@ int main() {
     cbreak();
     noecho();
     curs_set(0); // Hide cursor
-
     w = newwin(LINES, COLS, 0, 0);
     if (LINES < 30 || COLS < 80) terminal_too_small_screen(w);
 
     splash_screen(w);
     //game_over_screen(w);
     //victory_screen(w);
-    display_level(w, 1);
-    getch();
-    display_level(w, 2);
-    getch();
-    display_level(w, 3);
-    getch();
+    while(1){
+	    
+	if(ch == 'q'){
+	    quit_screen(w);
+	    ch = getch();
+	    if(ch == 'y'){
+            	break;
+	    }
+	    else if(ch == 'n'){
+	    	continue;
+		//go back to original screen level
+	    }
+	}
+
+	else if(ch == 'p'){
+	    pause_screen(w);
+	    while(1){
+	    	getch();
+		if(ch == 'p'){
+		    break;
+		}
+	    }
+	    //go back to original screen level
+	}
+        ch = getch();
+    }
     endwin();
     return 0;
 }
